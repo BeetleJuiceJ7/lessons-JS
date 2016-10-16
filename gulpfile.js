fc50cde772103e8c13d433d00bdad4f338eac291
Beetle Juice <kursksilya@gmail.com>
2016-10-16 17:59:36 +0300

    install_minify_css

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,8 @@
 var gulp = require('gulp');
-	concatCss = require('gulp-concat-css'),
-	rename = require('gulp-rename'),
-	minifyCSS = require('gulp-minify-css'),
-	notify = require('gulp-notify'),
-    autoprefixer = require('gulp-autoprefixer'),
-    postCSS = require('gulp-postcss'),
-   // sourcemaps = require('gulp-sourcemaps');
+	concatCss = require('gulp-concat-css');
 
 gulp.task('default', function () {
-   gulp.src('css/*.css')
+  return gulp.src('css/*.css')
     .pipe(concatCss('bundle.css'))
-    .pipe(rename('bundle-min.css'))
-    .pipe(minifyCSS())
-    .pipe(sourcemaps.init())
-    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-    //.pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('app/css'))
-    .pipe(notify('Done!'));
+    .pipe(gulp.dest('app/'));
 });
-
-gulp.task('watch', function () {
-	gulp.watch('css/*.css', ['default'])
-})
