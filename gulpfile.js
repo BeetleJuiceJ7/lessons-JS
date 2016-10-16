25406e682f72ba32e7d59294b2494db4bd61be4a
Beetle Juice <kursksilya@gmail.com>
2016-10-16 21:32:12 +0300

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ var gulp = require('gulp');
 	concatCss = require('gulp-concat-css'),
     rename = require('gulp-rename'),
     minifyCSS = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
     notify = require('gulp-notify');
 
 gulp.task('default', function () {
    gulp.src('css/*.css')
-    .pipe(concatCss('css/bundle.css'))
+    .pipe(concatCss('bundle.css'))
     .pipe(minifyCSS())
-    .pipe(rename('css/bundle.min.css'))
+    .pipe(rename('bundle.min.css'))
+    .pipe(autoprefixer('last 15 versions'))
     .pipe(notify('Done!'))
-    .pipe(gulp.dest('app/'));
+    .pipe(gulp.dest('app/css'));
 });
 
 gulp.task('watch', function(){
