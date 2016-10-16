f517d2185f2f9c957b6f11960338dcb06d156e1f
Beetle Juice <kursksilya@gmail.com>
2016-10-16 16:21:38 +0300

    settings gulp end

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
-	var = concatCSS = require('gulp-concat-css'),
+	concatCss = require('gulp-concat-css'),
 	rename = require('gulp-rename'),
-	minifyCSS = require('gulp-minify-css');
+	minifyCSS = require('gulp-minify-css'),
+	notify = require('gulp-notify');
 
 gulp.task('default', function () {
-  return gulp.src('css/*.css')
-    .pipe(concatCSS('bundle.css'))
+   gulp.src('css/*.css')
+    .pipe(concatCss('bundle.css'))
     .pipe(rename('bundle-min.css'))
-    .pipe(nminifyCSS(''))
-    .pipe(gulp.dest('app/'));
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('app/css'))
+    .pipe(notify('Done!'));
 });
+
+gulp.task('watch', function () {
+	gulp.watch('css/*.css', ['default'])
+})
