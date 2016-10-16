a4b18d1befa6d6a4741f716d54cf5b1d7d6af21a
Beetle Juice <kursksilya@gmail.com>
2016-10-16 17:45:37 +0300

    fuck

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@ var gulp = require('gulp');
 	concatCss = require('gulp-concat-css'),
 	rename = require('gulp-rename'),
 	minifyCSS = require('gulp-minify-css'),
-	notify = require('gulp-notify');
+	notify = require('gulp-notify'),
+    autoprefixer = require('gulp-autoprefixer'),
+    postCSS = require('gulp-postcss'),
+   // sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function () {
    gulp.src('css/*.css')
     .pipe(concatCss('bundle.css'))
     .pipe(rename('bundle-min.css'))
     .pipe(minifyCSS())
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    //.pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('app/css'))
     .pipe(notify('Done!'));
 });
