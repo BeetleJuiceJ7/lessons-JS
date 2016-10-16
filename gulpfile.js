b2495900bdf468f33a4a6dcc5d6c4b842a77cff2
Beetle Juice <kursksilya@gmail.com>
2016-10-16 22:06:24 +0300

    gulp_connect_autoprefexer_livereload

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,45 @@ var gulp = require('gulp');
     rename = require('gulp-rename'),
     minifyCSS = require('gulp-minify-css'),
     autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect'),
     notify = require('gulp-notify');
 
-gulp.task('default', function () {
+
+// server connect
+    gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true
+  });
+});
+
+
+//css style
+gulp.task('css', function () {
    gulp.src('css/*.css')
     .pipe(concatCss('bundle.css'))
     .pipe(minifyCSS())
     .pipe(rename('bundle.min.css'))
     .pipe(autoprefixer('last 15 versions'))
-    .pipe(notify('Done!'))
-    .pipe(gulp.dest('app/css'));
+   // .pipe(notify('Done!'))
+    .pipe(gulp.dest('app/css'))
+    .pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
-    gulp.watch('css/*.css', ['default'])
+//html
+gulp.task('html', function () {
+    gulp.src('app/index.html')
+    .pipe(connect.reload());
 })
+
+//gulp-watch
+gulp.task('watch', function () {
+    gulp.watch('css/*.css', ['css'])
+    gulp.watch('app/index.html', ['html'])
+})
+
+
+//default
+
+gulp.task('default', ['connect', 'html', 'css', 'watch']);
